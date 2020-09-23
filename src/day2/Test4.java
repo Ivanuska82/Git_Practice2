87a6a0bad2f3815153150b5d88cde28598acfc59
Ivanuska82 <66562415+Ivanuska82@users.noreply.github.com>
2020-09-23 12:10:18 -0400

    Update Test4.java
    Changes made to gitHub

--- src/day2/Test4.java
+++ src/day2/Test4.java
@@ -3,5 +3,7 @@ package day2;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("Something");
+        System.out.println("Success lol");
+            
     }
 }
